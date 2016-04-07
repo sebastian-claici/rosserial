82dbd2ec8796b8ca410b8b3a3f0aaf6953af737c
Sebastian Claici <sebastianclaici@gmail.com>
2016-04-07 15:21:56 -0400

    [FIX]: Added ESP8266 WiFi support to rosserial.
    To use in your Arduino project:
    
    ```
    \#define USE_WIFICON
    \#include <ESP8266WiFi.h>
    ```
    
    Nodes should be initialized using `nh.initNode(char *hostname)`.

--- rosserial_arduino/src/ros_lib/ArduinoHardware.h
+++ rosserial_arduino/src/ros_lib/ArduinoHardware.h
@@ -36,77 +36,115 @@
 #define ROS_ARDUINO_HARDWARE_H_
 
 #if ARDUINO>=100
-  #include <Arduino.h>  // Arduino 1.0
+#include <Arduino.h>  // Arduino 1.0
 #else
-  #include <WProgram.h>  // Arduino 0022
+#include <WProgram.h>  // Arduino 0022
 #endif
 
 #if defined(__MK20DX128__) || defined(__MK20DX256__)
-  #if defined(USE_TEENSY_HW_SERIAL)
-    #define SERIAL_CLASS HardwareSerial // Teensy HW Serial
-  #else
-    #include <usb_serial.h>  // Teensy 3.0 and 3.1
-    #define SERIAL_CLASS usb_serial_class
-  #endif
+#if defined(USE_TEENSY_HW_SERIAL)
+#define SERIAL_CLASS HardwareSerial // Teensy HW Serial
+#else
+#include <usb_serial.h>  // Teensy 3.0 and 3.1
+#define SERIAL_CLASS usb_serial_class
+#endif
 #elif defined(_SAM3XA_)
-  #include <UARTClass.h>  // Arduino Due
-  #define SERIAL_CLASS UARTClass
+#include <UARTClass.h>  // Arduino Due
+#define SERIAL_CLASS UARTClass
 #elif defined(USE_USBCON)
-  // Arduino Leonardo USB Serial Port
-  #define SERIAL_CLASS Serial_
+// Arduino Leonardo USB Serial Port
+#define SERIAL_CLASS Serial_
+#elif defined(USE_WIFICON)
+// Connection to TCP socket
+#include <ESP8266WiFi.h>
+#define SERIAL_CLASS WiFiClient
+#if !defined(ROSSERIAL_PORTNUM)
+#define ROSSERIAL_PORTNUM 11411
+#endif
 #else 
-  #include <HardwareSerial.h>  // Arduino AVR
-  #define SERIAL_CLASS HardwareSerial
+#include <HardwareSerial.h>  // Arduino AVR
+#define SERIAL_CLASS HardwareSerial
 #endif
 
 class ArduinoHardware {
-  public:
-    ArduinoHardware(SERIAL_CLASS* io , long baud= 57600){
-      iostream = io;
-      baud_ = baud;
-    }
-    ArduinoHardware()
-    {
+public:
+  ArduinoHardware(SERIAL_CLASS* io , long baud= 57600){
+    iostream = io;
+    baud_ = baud;
+  }
+  ArduinoHardware()
+  {
 #if defined(USBCON) and !(defined(USE_USBCON))
-      /* Leonardo support */
-      iostream = &Serial1;
+    /* Leonardo support */
+    iostream = &Serial1;
 #elif defined(USE_TEENSY_HW_SERIAL)
-      iostream = &Serial1;
+    iostream = &Serial1;
+#elif !defined(USE_WIFICON)
+    iostream = &Serial;
+#endif
+    baud_ = 57600;
+  }
+  ArduinoHardware(ArduinoHardware& h){
+    this->iostream = iostream;
+    this->baud_ = h.baud_;
+  }
+  
+  void setBaud(long baud){
+    this->baud_= baud;
+  }
+  
+  int getBaud(){return baud_;}
+
+#if defined(USE_WIFICON)
+  void init(char *hostname) {
+    iostream = new WiFiClient();
+    strcpy(this->hostname, hostname);
+
+    int tries = 0;
+    while (!iostream->connect(hostname, ROSSERIAL_PORTNUM) && tries < 50) {
+      delay(50);
+      tries += 1;
+    }
+  }
 #else
-      iostream = &Serial;
-#endif
-      baud_ = 57600;
-    }
-    ArduinoHardware(ArduinoHardware& h){
-      this->iostream = iostream;
-      this->baud_ = h.baud_;
-    }
-  
-    void setBaud(long baud){
-      this->baud_= baud;
-    }
-  
-    int getBaud(){return baud_;}
-
-    void init(){
+  void init(){
 #if defined(USE_USBCON)
-      // Startup delay as a fail-safe to upload a new sketch
-      delay(3000); 
+    // Startup delay as a fail-safe to upload a new sketch
+    delay(3000); 
 #endif
-      iostream->begin(baud_);
+    iostream->begin(baud_);
+  }
+#endif
+
+  int read(){return iostream->read();};
+  void write(uint8_t* data, int length){
+    for(int i=0; i<length; i++)
+      iostream->write(data[i]);
+  }
+
+  unsigned long time(){return millis();}
+
+#if defined(USE_WIFICON)
+  bool isConnectionUp() {
+    if (iostream->connected()) {
+      return true;
+    } else {
+      return false;
     }
+  }
 
-    int read(){return iostream->read();};
-    void write(uint8_t* data, int length){
-      for(int i=0; i<length; i++)
-        iostream->write(data[i]);
-    }
+  bool restoreConnection() {
+    iostream->stop();
+    return iostream->connect(this->hostname, ROSSERIAL_PORTNUM);
+  }
+#endif
 
-    unsigned long time(){return millis();}
-
-  protected:
-    SERIAL_CLASS* iostream;
-    long baud_;
+protected:
+#if defined(USE_WIFICON)
+  char hostname[256];
+#endif
+  SERIAL_CLASS* iostream;
+  long baud_;
 };
 
 #endif
